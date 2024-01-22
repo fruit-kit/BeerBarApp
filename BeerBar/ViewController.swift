e1cd3e9a6b12352aa62ca376f58c2b7b845f5bd6
fruit-kit <kotrutsa666@gmail.com>
2024-01-22 15:37:02 +0200

    Initial commit

---
+++ BeerBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeerBar
+//
+//  Created by Robert Kotrutsa on 22.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
