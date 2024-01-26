df2dbb6adadd5e78800bfa5f1e945c2ec9328d59
fruit-kit <kotrutsa666@gmail.com>
2024-01-26 13:38:05 +0200

    Add "About Developer" Button and Setup UI Components: Labels, Buttons for StaffManagementVC

--- BeerBar/StaffManagementVC.swift
+++ BeerBar/StaffManagementVC.swift
@@ -16,11 +16,53 @@ class StaffManagementVC: UIViewController {
     @IBOutlet weak var totalRevenueLabel: UILabel!
     @IBOutlet weak var resetButton: UIButton!
     
+    @IBOutlet weak var aboutDeveloperButton: UIButton!
+    
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
+        
+        setupLabels()
+        setupButtons()
         super.viewDidLoad()
+    }
+    
+    private func setupLabels() {
+        
+        setupTodayRevenueLabel()
+        setupTotalRevenueLabel()
+    }
 
+    private func setupTodayRevenueLabel() {
+        
+        todayRevenueLabel.text = "Today's Revenue: \n$0.00"
+    }
+
+    private func setupTotalRevenueLabel() {
+        
+        totalRevenueLabel.text = "Total Revenue: \n$0.00"
+    }
+    
+    private func setupButtons() {
+        
+        setupNewShiftButton()
+        setupResetButton()
+        setupAboutDeveloperButton()
+    }
+
+    private func setupNewShiftButton() {
+        
+        newShiftButton.setTitle("New Shift", for: .normal)
+    }
+
+    private func setupResetButton() {
+        
+        resetButton.setTitle("Reset", for: .normal)
+    }
+
+    private func setupAboutDeveloperButton() {
+        
+        aboutDeveloperButton.setTitle("About Developer", for: .normal)
     }
     
     // MARK: - Actions
@@ -30,6 +72,10 @@ class StaffManagementVC: UIViewController {
     }
     
     @IBAction func resetButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func aboutDeveloperButtonPressed(_ sender: UIButton) {
         
     }
     
