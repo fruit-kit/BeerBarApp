e3717e52831d5233d7ffc3c76650d6a9140863f2
fruit-kit <kotrutsa666@gmail.com>
2024-01-31 10:05:01 +0200

    Add BeerModel

---
+++ BeerBar/BeerModel.swift
@@ -0,0 +1,22 @@
+//
+//  BeerModel.swift
+//  BeerBar
+//
+//  Created by Robert Kotrutsa on 31.01.2024.
+//
+
+import UIKit
+
+struct Beer {
+    
+    let name: String
+    let country: String
+    let type: BeerType
+    let prices: (Decimal, Decimal, Decimal)
+    let image: UIImage
+}
+
+enum BeerType {
+    
+    case light, dark
+}
